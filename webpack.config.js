c5afdd463b909f0e4969eab0050c5fc95ebef891
Steve Mullins <sgmullins@gmail.com>
2020-02-11 15:15:27 -0700

    add redirects

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPLugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
@@ -19,6 +20,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'app/index.html',
     }),
+    new CopyPLugin([{ from: '_redirects' }]),
   ],
   devServer: {
     historyApiFallback: true,
